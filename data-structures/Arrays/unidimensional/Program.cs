f696227db1fd904cd164d5931d4a2a3521bcc23d
Javier Andrés Villabón Morales <javillabonmo@gmail.com>
2025-09-26 12:01:51 -0500

    Arrays unidimensionales

---
+++ data-structures/Arrays/unidimensional/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+
+namespace unidimensional
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Fundamentos programacion | quinta edicion - luis joyanes aguilar
+            //pag 264
+
+            string[] carNames = new string[8];
+            carNames[0] = "Alfa Romeo";
+            carNames[1] = "Fiat";
+            carNames[2] = "Ford";
+            carNames[3] = "Lancia";
+            carNames[4] = "Renault";
+            carNames[5] = "Seat";
+
+            int i = 0;
+            foreach (var name in carNames)
+            {
+                if (name != null)
+                    i++;
+                else
+                {
+                    carNames[i] = "null";
+                }
+            }
+
+            string[] elements = { "Opel", "Citroen" };
+
+            Program.InsertElement(4, elements[0], carNames, ref i);
+            Program.InsertElement(1, elements[1], carNames, ref i);
+
+            Program.DeleteElement(3, carNames, ref i);
+            Program.DeleteElement(2, carNames, ref i);
+
+
+            foreach (string item in carNames)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+
+        public static void InsertElement(int position, string element, string[] list, ref int i)
+        {
+            int tmp = i - 1;
+
+            while (tmp >= position)
+            {
+
+                list[tmp + 1] = list[tmp];
+                tmp--;
+            }
+
+            list[position] = element;
+            i++;
+        }
+
+        public static void DeleteElement(int position, string[] list, ref int i)
+        {
+            i--;
+
+            while (position <= i)
+            {
+                if (position == i)
+                {
+                    list[i] = "null";
+                }
+                else
+                {
+                    list[position] = list[position + 1];
+                }
+
+
+                position++;
+            }
+
+        }
+    }
+
+}
