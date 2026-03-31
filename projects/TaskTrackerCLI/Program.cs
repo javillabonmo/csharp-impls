047efa61ba61c1d9d206af39fd4ceb6afff63eea
javillabonmo <javillabonmo@gmail.com>
2026-03-31 14:27:12 -0700

    Learning the API

---
+++ projects/TaskTrackerCLI/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace TaskTrackerCLI
+
+{
+    using System.CommandLine;
+    using System.CommandLine.Parsing;
+    using System.Threading.Tasks;
+    
+    internal 
+        class Program
+    {
+
+    static async Task<int> Main(string[] args)
+        {
+            RootCommand rootCommand = new("Sample app for System.CommandLine");
+
+            Argument<string> addArgument = new("add")
+            {
+                Description = "A positional argument that receives a task."
+            };
+
+            Command addCommand = new("add", "Add an entry to the file.");
+            addCommand.SetAction((parseResult) =>
+            {
+                string? result = parseResult.GetValue(addArgument);
+                Console.WriteLine($"You entered: {result}");
+
+            });
+
+            addCommand.Arguments.Add(addArgument);
+
+            rootCommand.Subcommands.Add(addCommand);
+
+
+            return rootCommand.Parse(args).Invoke();
+
+        }
+        
+    }
+
+}
