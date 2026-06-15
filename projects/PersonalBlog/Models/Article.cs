1216d131856a0d077aab38c2fc6d10b626d810dd
Javier Andrés Villabón Morales <javillabonmo@gmail.com>
2026-06-15 11:01:36 -0700

    stylecop

--- projects/PersonalBlog/Models/Article.cs
+++ projects/PersonalBlog/Models/Article.cs
@@ -1,19 +1,25 @@
-using System.ComponentModel.DataAnnotations;
-using PersonalBlog.Models.Common;
-
 namespace PersonalBlog.Models
 {
+    using System.ComponentModel.DataAnnotations;
+    using PersonalBlog.Models.Common;
+
+    /// <summary>
+    /// Representa un artículo en el blog, con propiedades para el título, contenido y metadatos de auditoría.
+    /// </summary>
     public class Article : AuditableEntityBase
     {
         [Key]
-        public int ArticleId {get;set;}
+        public int ArticleId { get;set; }
+
         [Required]
         [Display(Name = "Titulo")]
         [MaxLength(100)]
-        public required string Title {get; set; }
+        required public string Title { get; set; }
+
         [Display(Name = "Contenido")]
         [MaxLength(5000)]
         public string? Content {get;set;}
+
     }
 }
 
