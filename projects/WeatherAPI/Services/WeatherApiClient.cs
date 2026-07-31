89a82d997afbbb46827003a23a974a754618b860
Javier Andrés Villabón Morales <javillabonmo@gmail.com>
2026-07-31 16:17:14 -0700

    fix(weather-api): remove unused usings and fix brace in WeatherApiClient

--- projects/WeatherAPI/Services/WeatherApiClient.cs
+++ projects/WeatherAPI/Services/WeatherApiClient.cs
@@ -1,5 +1,3 @@
-using System.Net;
-using System.Net.Http.Json;
 using Microsoft.Extensions.Options;
 
 using WeatherAPI.Exceptions;
@@ -47,5 +45,5 @@ public sealed class WeatherApiClient : IWeatherApiClient
             throw ex.AddData("Location", location);
         }
         }
-       }
 }
+
